edb1e0aafd948d181385fba5c2c12f2b2cd5dc8b
CorraMatte <corra.matteo@gmail.com>
2019-06-05 22:08:58 +0200

    Add custom password and fix bugs

--- scripts/create_archives.py
+++ scripts/create_archives.py
@@ -7,68 +7,58 @@ import string
 import tarfile
 
 NPROC = 10
-ARCHIVE_DIR = os.path.abspath(os.path.join(os.pardir, 'archives'))
-TMP_DIR = 'tmp'
-
-
-def get_pass(string_length=10):
-    letters = string.ascii_lowercase + string.digits
-    return ''.join(
-        random.choice(letters) for i in range(random.randint(4, string_length)))
+ARCHIVE_DIR = '/tmp/' # os.path.abspath(os.path.join(os.pardir, 'archives'))
+TEMP_DIR = 'tmp'
 
 
 def create_single_file(args):
-    f = open('{}/tmp_{}.txt'.format(ARCHIVE_DIR, mp.current_process().pid), 'w')
-    words = open('words.txt', 'r').read().splitlines()
+    f = open('{}/tmp_{}.txt'.format(TEMP_DIR, mp.current_process().pid), 'w')
+    words = open('email.txt', 'r').read().splitlines()
+    pwds = open('password.txt', 'r').read().splitlines()
 
-    for i in range(500000):
+    for i in range(5000000):
         if i % 100 == 0:
-            f.writelines('{}:{}\n'.format(args, get_pass()))
-        f.writelines(
-            '{}@testemail.it:{}\n'.format(words[i % len(words)], get_pass()))
+            f.writelines('{}:{}'.format(args, pwds[i % len(pwds)]))
+        f.writelines('{}@testemail.it:{}\n'.format(words[i % len(words)], pwds[i % len(pwds)]))
 
     f.flush()
 
 
 def create_archive(email):
-    os.mkdir(TMP_DIR)
+    if (!os.path.exists(TEMP_DIR)):
+        os.mkdir(TEMP_DIR)
     p = mp.Pool(processes=10)
     args = [email for i in range(NPROC)]
+
+    print("Creating the text files..")
     p.map(create_single_file, args)
 
-    with tarfile.open('{}/archive_{}.tar.gz'.format(ARCHIVE_DIR,
-                                                    mp.current_process().pid),
-                      "w:gz") as tar:
-        tar.add(TMP_DIR, arcname=os.path.basename(TMP_DIR))
-        shutil.rmtree(TMP_DIR)
+    print("Creating the archive..")
+    with tarfile.open('{}/archive_{}.tar.gz'.format(ARCHIVE_DIR, mp.current_process().pid), "w:gz") as tar:
+        tar.add(TEMP_DIR, arcname=os.path.basename(TEMP_DIR))
+        shutil.rmtree(TEMP_DIR)
 
 
 def main():
-    parser = argparse.ArgumentParser(
-        description='Create the archives for the game')
-    parser.add_argument('-n', '--nfiles', type=int, default=4,
-                        help='Number of file to be created')
-    parser.add_argument('-e', '--email', required=True,
-                        help='Email to create in order to be found during the game')
+    parser = argparse.ArgumentParser(description='Create the archives for the game')
+    parser.add_argument('-n', '--nfiles', type=int, default=4, help='Number of file to be created')
+    parser.add_argument('-e', '--email', required=True, help='Email to create in order to be found during the game')
     args = parser.parse_args()
     nfiles = args.nfiles
 
-    print("This program will be create {} of the size of 500MB each".format(
-        nfiles))
+    print("This program will be create {} of 400MB each".format(nfiles))
     print("Continue?  [y/N]", end='')
     choose = input()
 
     if choose.upper() != 'Y':
         print("Nothing will be created")
         return
-
-    print("Creating files..")
-
+    
     for i in range(nfiles):
         print("Creating {}/{}".format(i + 1, nfiles))
         create_archive(args.email)
 
-    print("Files creates")
+    print("Archive creates")
 
 
 if __name__ == '__main__':
